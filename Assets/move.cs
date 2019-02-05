aff087d8024117931d22e1f89b9ac070fd25c9f6
Brandon Pagan <pagan.48@osu.edu>
2019-02-05 08:39:27 -0500

    Added Scene with:
     Basic HUD with health bar;
    Enemy health bar that stays above enemy as they move;
    Player and enemy take damage on contact and is visually represented on the health bars;

---
+++ Assets/move.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Rigidbody rb;
+    public float speed;
+    playerHealth playerHealth;
+
+
+
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody>();
+        playerHealth = GetComponent<playerHealth>();
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        enemy_stats health;
+
+        if (health = other.GetComponent<enemy_stats>())
+        {
+            health.ChangeHealth(-6);
+            playerHealth.ChangeHealth(-4);
+        }
+    }
+
+
+}
