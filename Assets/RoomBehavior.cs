3b35a5314f9fca07b5a8988c390410bdf09fe7bf
Cassandra Mendez <cfm555@gmail.com>
2019-02-15 12:05:01 -0500

    started room animation stuff

---
+++ Assets/RoomBehavior.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomBehavior : MonoBehaviour
+{
+    public enum RoomState { active, warning, crumbling, inactive };
+    public RoomState currentState;
+    private GameObject[] players;
+    private GameObject nearbyPlayer;
+    private ParticleSystem rocks;
+    private float stateTime, timeThreshhold;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = RoomState.active;
+        players = GameObject.FindGameObjectsWithTag("Player");
+        stateTime = 0f;
+        timeThreshhold = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        stateTime += Time.deltaTime;
+        if(nearbyPlayer != null && stateTime > timeThreshhold)
+        {
+            if(currentState == RoomState.active)
+            {
+                currentState = RoomState.warning;
+            }
+            else if(currentState == RoomState.warning)
+            {
+                //particles of small rocks 
+                currentState = RoomState.crumbling;
+            }
+            else if (currentState == RoomState.crumbling)
+            {
+                //particles of large rocks for threshhold
+                currentState = RoomState.inactive;
+            }
+            stateTime = 0;
+        }
+    }
+}
