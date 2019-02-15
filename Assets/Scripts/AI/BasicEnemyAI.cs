8647f05c5249d3153bea8646c82336d0da093608
tristo999 <tristanfitzwater@gmail.com>
2019-02-14 20:50:39 -0500

    Basic Enemy AI

--- Assets/Scripts/AI/BasicEnemyAI.cs
+++ Assets/Scripts/AI/BasicEnemyAI.cs
@@ -8,13 +8,14 @@ public class BasicEnemyAI : MonoBehaviour
 {
     public NavMeshAgent nav;
     public GameObject currentPlayer;
-    private Vector3 intPosition;
+    public Vector3 intPosition;
     private GameObject[] players;
     public enum enemyState
     {
         idle, chasing, attacking, returning
     };
     public enemyState currentState;
+    public float roomWidth = 30;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,21 +23,36 @@ public class BasicEnemyAI : MonoBehaviour
         players = GameObject.FindGameObjectsWithTag("Player");
         intPosition = transform.position;
         currentState = enemyState.idle;
-
     }
 
     // Update is called once per frame
     void Update()
     {
 
+        currentPlayer = null;
+        foreach (GameObject x in players)
+        {
+            if (x.transform.position.x < intPosition.x + (roomWidth / 2) && x.transform.position.x > intPosition.x - (roomWidth / 2) && x.transform.position.z < (intPosition.z + roomWidth) / 2 && x.transform.position.z > intPosition.z - (roomWidth / 2))
+            {
+                if (currentPlayer == null)
+                {
+                    currentPlayer = x;
+                }
+                else
+                {
+
+                }
+            }
+        }
         if (currentPlayer != null)
         {
             //Check range of player and decide to attack or chase
+
             nav.SetDestination(currentPlayer.transform.position);
         }
         else
         {
-            // If not at inital position, begin returning to position. If at current position then idle
+            nav.SetDestination(intPosition);
         }
     }
 }
