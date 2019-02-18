31fff8fb55a2751aeda27c3d9fe007836bde0d2f
tristo999 <tristanfitzwater@gmail.com>
2019-02-17 22:02:45 -0500

    Networked Animator

--- Assets/Scripts/Enemies/BasicEnemyAI.cs
+++ Assets/Scripts/Enemies/BasicEnemyAI.cs
@@ -10,141 +10,116 @@ public class BasicEnemyAI : Bolt.EntityEventListener<IEnemyState>
     public GameObject currentPlayer;
     public Vector3 intPosition;
     private GameObject[] players;
-    public enum enemyState
+    public bool inAttackAnim
     {
-        idle, chasing, attacking, returning
-    };
-    public enemyState currentState;
+        get
+        {
+            return enemyAnimator.GetCurrentAnimatorStateInfo(0).IsName("Attacking");
+        }
+    }
+
     public float roomWidth = 30;
-    public float attackTimer = 0f;
+    public float attackTimer = 6f;
     public float attackCooldown = 6f;
     public float animationLength = 3f;
     public float animationTimer = 0f;
     public bool inAttackRange;
     public bool inHitRange;
     public Animator enemyAnimator;
-    // Start is called before the first frame update
+
     public override void Attached()
     {
         state.SetTransforms(state.transform, transform);
+        enemyAnimator = GetComponentInChildren<Animator>();
+        state.SetAnimator(enemyAnimator);
+
         if (!entity.isOwner) return;
+        state.OnAttack += Attack;
         nav = this.GetComponent<NavMeshAgent>();
         players = GameObject.FindGameObjectsWithTag("Player");
         intPosition = transform.position;
-        currentState = enemyState.idle;
         attackTimer = 100f;
         animationTimer = 0f;
         inAttackRange = false;
         inHitRange = false;
-        enemyAnimator = GetComponentInChildren<Animator>();
-        enemyAnimator.SetInteger("Animation", 0);
+    }
+
+    private void Attack()
+    {
+        Debug.Log("Enemy attacking.");
+        attackTimer = 0f;
+        nav.isStopped = true;
     }
 
     // Update is called once per frame
     public override void SimulateOwner()
     {
-        switch (currentState)
-        {
-            case enemyState.idle:
-                findCurrentPlayer();
-                if (currentPlayer != null)
-                {
-                    currentState = enemyState.chasing;
-                    enemyAnimator.SetInteger("Animation", 1);
-                }
-                break;
-            case enemyState.chasing:
-                findCurrentPlayer();
-                if (currentPlayer != null)
-                {
-                    enemyAnimator.SetInteger("Animation", 1);
-                    if (inAttackRange)
-                    {
-                        if (attackTimer > attackCooldown)
-                        {
-                            currentState = enemyState.attacking;
-                            enemyAnimator.SetInteger("Animation", 2);
-                            nav.SetDestination(transform.position);
-                            attackTimer = 0;
-                        } else
-                        {
-                            enemyAnimator.SetInteger("Animation", 0);
-                        }
-                    }
-                    else
-                    {
-                        nav.SetDestination(currentPlayer.transform.position);
-                    }
-                }
-                else
-                {
-                    currentState = enemyState.returning;
-                    enemyAnimator.SetInteger("Animation", 1);
-                }
-                break;
-            case enemyState.attacking:
-                if (animationTimer > animationLength)
-                {
-                    if (inHitRange)
-                    {
-                        //Do Damage
-                    }
-                    currentState = enemyState.idle;
-                    animationTimer = 0;
-                    enemyAnimator.SetInteger("Animation", 0);
-                } else
-                {
-                    animationTimer += Time.deltaTime;
-                    enemyAnimator.SetInteger("Animation", 0);
-                }
-                break;
+        //state.Animation = (int)currentState;
+        currentPlayer = findCurrentPlayer();
+        state.Moving = !nav.isStopped;
+        CheckAttack();
+        CheckMove();
 
-            case enemyState.returning:
-                findCurrentPlayer();
-                                        nav.SetDestination(intPosition);
-                if (currentPlayer)
-                {
-                    currentState = enemyState.chasing;
-                    enemyAnimator.SetInteger("Animation", 1);
-                }
-                else
-                {
-                    if (!nav.pathPending)
-                    {
-                        if (nav.remainingDistance <= nav.stoppingDistance)
-                        {
-                            if (!nav.hasPath || nav.velocity.sqrMagnitude == 0f)
-                            {
-                                currentState = enemyState.idle;
-                                enemyAnimator.SetInteger("Animation", 0);
-                            }
-                        }
-                    }
-                }
-                break;
-        }
-        attackTimer += Time.deltaTime;
+        if (attackTimer < attackCooldown)
+            attackTimer += BoltNetwork.FrameDeltaTime;
     }
 
-    private void findCurrentPlayer()
+    private void CheckAttack()
     {
-        currentPlayer = null;
+        if (!inAttackAnim && inAttackRange)
+        {
+            //Do Damage.
+        }
+
+        if (currentPlayer && inAttackRange && attackTimer > attackCooldown)
+        {
+            state.Attack();
+        }
+    }
+
+    private void CheckMove()
+    {
+        if (inAttackAnim || enemyAnimator.IsInTransition(0)) return;
+
+        if (currentPlayer && !inAttackRange)
+        {
+            nav.SetDestination(currentPlayer.transform.position);
+            nav.isStopped = false;
+        }
+
+        if (!currentPlayer)
+        {
+            nav.SetDestination(intPosition);
+            if (nav.remainingDistance <= nav.stoppingDistance)
+            {
+                nav.isStopped = true;
+            } else
+            {
+                nav.isStopped = false;
+            }
+        }
+    }
+
+    private GameObject findCurrentPlayer()
+    {
+        GameObject pObj = null;
         foreach (GameObject x in players)
         {
             if (x.transform.position.x < intPosition.x + (roomWidth / 2) && x.transform.position.x > intPosition.x - (roomWidth / 2) && x.transform.position.z < (intPosition.z + roomWidth) / 2 && x.transform.position.z > intPosition.z - (roomWidth / 2))
             {
-                if (currentPlayer == null)
+                if (pObj == null)
                 {
-                    currentPlayer = x;
+                    pObj = x;
                 }
                 else
                 {
-                    if (Vector3.Distance(transform.position, currentPlayer.transform.position) > Vector3.Distance(transform.position, x.transform.position))
+                    if (Vector3.Distance(transform.position, pObj.transform.position) > Vector3.Distance(transform.position, x.transform.position))
                     {
-                        currentPlayer = x;
+                        pObj = x;
                     }
                 }
             }
         }
+        return pObj;
     }
 }
---
+++ Assets/TristanTest/AnimTestingCallbacks.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[BoltGlobalBehaviour(BoltNetworkModes.Server, "TestNavMesh")]
+public class AnimTestingCallbacks : Bolt.GlobalEventListener
+{
+    public override void SceneLoadLocalDone(string scene)
+    {
+        GameObject skele = BoltNetwork.Instantiate(BoltPrefabs.TestEnemy);
+        skele.transform.position = new Vector3(0, 1.71f, 0);
+    }
+}
