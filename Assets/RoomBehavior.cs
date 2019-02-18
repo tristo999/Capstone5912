b572c19fec0bb02b20f18529ab24405d59f941c2
Cassandra Mendez <cfm555@gmail.com>
2019-02-17 21:46:07 -0500

    room destruction rock particles
    (in cass destruction scene)

--- Assets/RoomBehavior.cs
+++ Assets/RoomBehavior.cs
@@ -1,20 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class RoomBehavior : MonoBehaviour
 {
     public enum RoomState { active, warning, crumbling, inactive };
     public RoomState currentState;
     private GameObject[] players;
-    private GameObject nearbyPlayer;
-    private ParticleSystem rocks;
+    public GameObject nearbyPlayer;
+    public GameObject smallRocksPrefab, bigRocksPrefab;
+    public TextMeshProUGUI deathText;
+    private ParticleSystem smallRocks, bigRocks;
     private float stateTime, timeThreshhold;
     // Start is called before the first frame update
     void Start()
     {
         currentState = RoomState.active;
         players = GameObject.FindGameObjectsWithTag("Player");
+
+        smallRocks = Instantiate(smallRocksPrefab).GetComponent<ParticleSystem>();
+        bigRocks = Instantiate(bigRocksPrefab).GetComponent<ParticleSystem>();
+        deathText.text = "";
+
+        smallRocks.transform.position = new Vector3(transform.position.x, transform.position.y + 9, transform.position.z);
+        bigRocks.transform.position = new Vector3(transform.position.x / 2, transform.position.y + 9, transform.position.z / 2);
+
         stateTime = 0f;
         timeThreshhold = 5f;
     }
@@ -23,21 +34,26 @@ public class RoomBehavior : MonoBehaviour
     void Update()
     {
         stateTime += Time.deltaTime;
-        if(nearbyPlayer != null && stateTime > timeThreshhold)
+        if(players[0] != null && stateTime > timeThreshhold)
         {
-            if(currentState == RoomState.active)
+            switch(currentState)
             {
-                currentState = RoomState.warning;
-            }
-            else if(currentState == RoomState.warning)
-            {
-                //particles of small rocks 
-                currentState = RoomState.crumbling;
-            }
-            else if (currentState == RoomState.crumbling)
-            {
-                //particles of large rocks for threshhold
-                currentState = RoomState.inactive;
+                case RoomState.active:
+                    currentState = RoomState.warning;
+                    break;
+                case RoomState.warning:
+                    //particles of small rocks 
+                    smallRocks.Play();
+                    currentState = RoomState.crumbling;
+                    break;
+                case RoomState.crumbling:
+                    //particles of large rocks for threshhold
+                    bigRocks.Play();
+                    currentState = RoomState.inactive;
+                    break;
+                case RoomState.inactive:
+                    deathText.text = "Death has Taken You";
+                    break;
             }
             stateTime = 0;
         }
