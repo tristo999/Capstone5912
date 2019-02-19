9e9f1ecd529f9c80978f1ff89d5f0cd117f40393
Brandon Pagan <pagan.48@buckeyemail.osu.edu>
2019-02-18 23:19:21 -0500

    Knight enemy with sword and animation system

---
+++ Assets/bpTest/BrandonMove.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrandonMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Rigidbody rb;
+    public float speed;
+    BrandonPlayerHealth playerHealth;
+
+
+
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody>();
+        playerHealth = GetComponent<BrandonPlayerHealth>();
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        BrandonEnemyHealth health;
+
+        if (health = other.GetComponent<BrandonEnemyHealth>())
+        {
+            health.ChangeHealth(-6);
+            playerHealth.ChangeHealth(-4);
+        }
+    }
+
+
+}
---
+++ Assets/bpTest/EnemyMove.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    Animator anim;
+    int speedHash;
+    Vector3 position;
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        speedHash = Animator.StringToHash("Speed");
+    }
+
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        anim.SetFloat(speedHash, moveHorizontal);
+
+
+        gameObject.transform.Translate(Vector3.forward * Time.deltaTime * moveHorizontal * 5f);
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+       
+    }
+
+    public void Attack()
+    {
+
+    }
+
+    public void TurnAround() {
+        
+
+
+    }
+
+}
