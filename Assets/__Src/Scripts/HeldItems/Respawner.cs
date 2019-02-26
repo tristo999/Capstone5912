60d2f6f218ac9e069dab905b6e2d6fb635e9cb08
Cassandra Mendez <cfm555@gmail.com>
2019-02-25 22:56:14 -0500

    Started room light stuff
    (only on room_1 for now)

---
+++ Assets/__Src/Scripts/HeldItems/Respawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawner : MonoBehaviour
+{
+    public bool debug = true;
+
+    GameObject SpawnObject(GameObject[] prefabs){
+        Transform pos = GetComponent<Transform>();
+        GameObject toSpawn = GameObject.Find("WallLightSpawner");
+        if (toSpawn.GetComponent<BoltEntity>())
+            return BoltNetwork.Instantiate(toSpawn, transform.position, Quaternion.identity);
+        else
+            return Instantiate(toSpawn, transform.position, Quaternion.identity);
+    }
+
+}
