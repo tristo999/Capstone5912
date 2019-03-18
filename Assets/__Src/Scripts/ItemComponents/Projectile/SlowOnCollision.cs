c3789b51e28aec9035d775348d9d3a7ef6b9514d
TreeStew <tristanfitzwater@gmail.com>
2019-03-18 14:07:26 -0400

    MovingToOtherPC

---
+++ Assets/__Src/Scripts/ItemComponents/Projectile/SlowOnCollision.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Rigidbody))]
+public class SlowOnCollision : Bolt.EntityBehaviour<IProjectileState>
+{
+    public float slowMultiplier;
+    private Rigidbody rigid;
+
+    public override void Attached()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!entity.isAttached || !entity.isOwner) return;
+        CollisionCheck checker = GetComponent<CollisionCheck>();
+        if (checker && !checker.ValidCollision(collision)) return;
+
+        if (collision.gameObject.GetComponent<BoltEntity>() && collision.gameObject.GetComponent<BoltEntity>().isAttached)
+        {
+            /*
+            KnockbackEntity KnockbackEntity = KnockbackEntity.Create(collision.gameObject.GetComponent<BoltEntity>());
+            KnockbackEntity.Force = rigid.velocity.normalized * knockback;
+            KnockbackEntity.Send();
+            */
+        }
+        else
+        {
+            /*
+            Rigidbody otherRigid = collision.gameObject.GetComponent<Rigidbody>();
+            if (otherRigid)
+            {
+                otherRigid.AddForce(rigid.velocity.normalized * knockback);
+            }
+            */
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!entity.isAttached || !entity.isOwner) return;
+        CollisionCheck checker = GetComponent<CollisionCheck>();
+        if (checker && !checker.ValidCollision(other)) return;
+        if (other.gameObject.GetComponent<BoltEntity>() && other.gameObject.GetComponent<BoltEntity>().isAttached)
+        { 
+            /*
+            KnockbackEntity KnockbackEntity = KnockbackEntity.Create(other.gameObject.GetComponent<BoltEntity>());
+            KnockbackEntity.Force = rigid.velocity.normalized * knockback;
+            KnockbackEntity.Send();
+            */
+        }
+        else
+        {
+            /*
+            Rigidbody otherRigid = other.GetComponent<Rigidbody>();
+            if (otherRigid)
+            {
+                otherRigid.AddForce(rigid.velocity.normalized * knockback);
+            }
+            */
+        }
+        
+    }
+}
---
+++ Assets/__Src/Scripts/Projectiles/IceWandProjectile.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceWandProjectile : Bolt.EntityBehaviour<IProjectileState>
+{
+    public override void Attached()
+    {
+        state.SetTransforms(state.transform, transform);
+    }
+}
