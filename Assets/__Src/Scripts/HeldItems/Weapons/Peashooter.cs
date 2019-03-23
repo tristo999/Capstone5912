2a59bf910b5be0f3dbc16bcd26b41a678f503401
rcologgi <cologgi.2@buckeyemail.osu.edu>
2019-03-23 18:38:48 -0400

    Added peashooter weapon

---
+++ Assets/__Src/Scripts/HeldItems/Weapons/Peashooter.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(WeaponCooldown))]
+[RequireComponent(typeof(WeaponLaunchProjectile))]
+public class Peashooter : Weapon {
+    private WeaponCooldown cooldown;
+    private WeaponLaunchProjectile launchProj;
+
+    public override void FireDown() {
+        cooldown = GetComponent<WeaponCooldown>();
+        launchProj = GetComponent<WeaponLaunchProjectile>();
+    }
+
+    public override void FireHold() {
+        if (!Owner.entity.isOwner) return;
+        if (cooldown.Ready) {
+            launchProj.Launch();
+            cooldown.ResetCooldown();
+        }
+    }
+
+    public override void FireRelease() {
+        
+    }
+
+    public override void OnEquip() {
+        
+    }
+}
