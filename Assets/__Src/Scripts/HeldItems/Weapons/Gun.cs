577f0314cdaba878f9f82cd6762065a562bd19b3
David Grougan <ddogclaw@gmail.com>
2019-03-28 20:38:43 -0400

    Gun

---
+++ Assets/__Src/Scripts/HeldItems/Weapons/Gun.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : Weapon
+{
+    public override void FireDown() {
+        
+    }
+
+    public override void FireHold() {
+        
+    }
+
+    public override void FireRelease() {
+        
+    }
+
+    public override void OnEquip() {
+        
+    }
+}
