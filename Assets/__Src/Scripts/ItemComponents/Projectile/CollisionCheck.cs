00227ed083f935191fa666a6fd92628329e28f7d
tristo999 <tristanfitzwater@gmail.com>
2019-03-30 17:56:42 -0400

    Fixed Collisions - TakyWand and Ice Wand

--- Assets/__Src/Scripts/ItemComponents/Projectile/CollisionCheck.cs
+++ Assets/__Src/Scripts/ItemComponents/Projectile/CollisionCheck.cs
@@ -9,18 +9,23 @@ public class CollisionCheck : Bolt.EntityBehaviour<IProjectileState>
     public bool CollideWithEnvironment;
 
     public bool ValidCollision(Collision collision) {
+
         BoltEntity collidedEntity = collision.gameObject.GetComponent<BoltEntity>();
         bool playerCheck = collision.gameObject.tag == "Player" && CollideWithPlayers;
         bool selfCheck = state.Owner == collidedEntity && CollideWithOwner;
-        bool envCheck = collision.gameObject.tag != "Player" && collision.gameObject.tag != "Projectile" && collision.gameObject.tag != "RangeDetector" && CollideWithEnvironment;
-        return playerCheck || selfCheck || envCheck;
+        bool envCheck = collision.gameObject.tag != "Player" && CollideWithEnvironment;
+        bool rangeColliderCheck = collision.gameObject.tag != "RangeDetector";
+        bool projectileCheck = collision.gameObject.tag != "Projectile";
+        return playerCheck || selfCheck || envCheck || rangeColliderCheck || projectileCheck;
     }
 
     public bool ValidCollision(Collider collider) {
         BoltEntity collidedEntity = collider.gameObject.GetComponent<BoltEntity>();
         bool playerCheck = collider.gameObject.tag != "Player" || CollideWithPlayers;
         bool selfCheck = state.Owner != collidedEntity || CollideWithOwner;
-        bool envCheck = collider.gameObject.tag != "Player" && collider.gameObject.tag != "Projectile" && collider.gameObject.tag != "RangeDetector" && CollideWithEnvironment;
-        return playerCheck && selfCheck && envCheck;
+        bool envCheck = collider.gameObject.tag != "Player" ||  CollideWithEnvironment;
+        bool rangeColliderCheck = collider.gameObject.tag != "RangeDetector";
+        bool projectileCheck = collider.gameObject.tag != "Projectile";
+        return playerCheck && selfCheck && envCheck && rangeColliderCheck && projectileCheck;
     }
 }
