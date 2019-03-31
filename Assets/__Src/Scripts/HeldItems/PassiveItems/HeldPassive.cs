52577eb97633815e601600938f66b8f66d4ff74d
Cassandra Mendez <cfm555@gmail.com>
2019-03-31 19:15:51 -0400

    initial implementation of heldpassive

--- Assets/__Src/Scripts/HeldItems/PassiveItems/HeldPassive.cs
+++ Assets/__Src/Scripts/HeldItems/PassiveItems/HeldPassive.cs
@@ -4,7 +4,48 @@ using UnityEngine;
 
 public class HeldPassive : HeldItem
 {
-    public virtual void OnEquip() {
+    private float speed;
+    private float health;
+    private float damage;
 
+    public virtual void OnEquip() {
+        GameObject obj = this.GetComponent<GameObject>();
+        ItemDefinition item = ItemManager.Instance.items[this.Id];
+        speed = item.SpeedModifier;
+        health = item.HealthModifier;
+        damage = item.DamageModifier;
+
+        // modify state with all modifiers
+        // speed
+        if(speed > 0)
+        {
+            // timed speed mod
+            Owner.state.Speed += speed;
+            float time = 0;
+            while(time < 10)
+            {
+                time += Time.deltaTime;
+            }
+            Owner.state.Speed -= speed;
+        }
+
+        // health
+        if(health > 0)
+        {
+            // add to health
+            Owner.state.Health += health;
+        }
+
+        // damage
+        if(damage > 0)
+        {
+            // take damage
+            Owner.state.Health -= damage;
+        }
+
+        //TODO add equip animation??
+
+        // destroy current
+        Destroy(obj);
     }
 }
