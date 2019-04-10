1c15cf58f5ccfdbe5c5064d1a922d6855bcc6b63
David Grougan <ddogclaw@gmail.com>
2019-04-10 16:45:16 -0400

    Debuff system

---
+++ Assets/PlayerDebuffHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDebuffHandler : Bolt.EntityBehaviour<IPlayerState>
+{
+    List<Debuff> playerDebuffs = new List<Debuff>();
+    PlayerUI ui;
+
+    public override void Attached() {
+        ui = GetComponent<PlayerUI>();
+    }
+
+    public void GrantDebuff(Debuff debuff) {
+        ui.AddFloatingMessageText("Got Debuff: " + debuff.Name, transform.position);
+        playerDebuffs.Add(debuff);
+        debuff.OnGiven(state);
+        debuff.GrantedTime = BoltNetwork.Time;
+    }
+
+    public void RemoveDebuff(Debuff debuff) {
+        ui.AddFloatingMessageText("Debuff Removed: " + debuff.Name, transform.position);
+        playerDebuffs.Remove(debuff);
+        debuff.OnRemoved(state);
+    }
+
+    public override void SimulateOwner() {
+        foreach (Debuff debuff in playerDebuffs) {
+            if (BoltNetwork.Time - debuff.GrantedTime > debuff.EffectLength) {
+                RemoveDebuff(debuff);
+            } else {
+                debuff.OnUpdate(state);
+            }
+        }
+    }
+}
