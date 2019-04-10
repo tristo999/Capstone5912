61dac4ef4d93b9fdbbcebb4b56ed57fd8d2f9051
tristo999 <tristanfitzwater@gmail.com>
2019-04-10 17:10:30 -0400

    Added Treasure Rooms

---
+++ Assets/__Src/Scripts/Debuffs/Debuff.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Debuff : MonoBehaviour
+{
+    public abstract string Name { get; set; }
+
+    public abstract float EffectLength { get; set; }
+
+    public float GrantedTime { get; set; }
+
+    public abstract void OnGiven(IPlayerState playerState);
+
+    public abstract void OnRemoved(IPlayerState playerState);
+
+    public abstract void OnUpdate(IPlayerState playerState);
+}
