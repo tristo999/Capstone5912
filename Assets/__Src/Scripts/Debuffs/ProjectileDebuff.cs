847a45fa35e135490b9ab35bc462e83df0130448
TreeStew <tristanfitzwater@gmail.com>
2019-04-10 20:45:19 -0400

    Added Debuff System to Projectiles

---
+++ Assets/__Src/Scripts/Debuffs/ProjectileDebuff.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileDebuff : Debuff
+{
+    public override string Name { get; set; }
+
+    public override float EffectLength { get; set; }
+
+    public float healthMod { get; set; }
+
+    public float speedMod { get; set; }
+
+    public float projectileMod { get; set; }
+
+    public float firerateMod { get; set; }
+
+    public float damageMod { get; set; }
+
+    public override void OnGiven(IPlayerState Owner)
+    {
+        if (!FloatRoughlyZero(healthMod))
+        {
+            Owner.Health += healthMod;
+        }
+        if (!FloatRoughlyZero(speedMod)) Owner.Speed += speedMod;
+        if (!FloatRoughlyZero(firerateMod)) Owner.FireRate += firerateMod;
+        if (!FloatRoughlyZero(projectileMod)) Owner.ProjectileSpeed += projectileMod;
+        if (!FloatRoughlyZero(damageMod)) Owner.ProjectileDamage += damageMod;
+    }
+
+    public  override void OnRemoved(IPlayerState Owner)
+    {
+        if (!FloatRoughlyZero(healthMod))
+        {
+            Owner.Health += healthMod;
+        }
+        if (!FloatRoughlyZero(speedMod)) Owner.Speed += speedMod;
+        if (!FloatRoughlyZero(firerateMod)) Owner.FireRate += firerateMod;
+        if (!FloatRoughlyZero(projectileMod)) Owner.ProjectileSpeed += projectileMod;
+        if (!FloatRoughlyZero(damageMod)) Owner.ProjectileDamage += damageMod;
+    }
+
+    public override void OnUpdate(IPlayerState playerState)
+    {
+
+    }
+
+    private bool FloatRoughlyZero(float val)
+    {
+        return Math.Abs(val) < 0.0001f;
+    }
+}
---
+++ Assets/__Src/Scripts/ItemComponents/Projectile/DebuffOnCollision.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(CollisionCheck))]
+
+public class DebuffOnCollision : Bolt.EntityBehaviour<IProjectileState>
+{
+    public bool damageOwner;
+    public string debuffName;
+    public float debuffDuration;
+    public float healthModifier;
+    public float speedModifier;
+    public float fireRateModifier;
+    public float projectileSpeedModifier;
+    public float damageModifier;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!entity.isAttached || !entity.isOwner) return;
+        BoltEntity otherEntity = collision.gameObject.GetComponent<BoltEntity>();
+        if (GetComponent<CollisionCheck>().ValidCollision(collision) && otherEntity && (damageOwner || otherEntity != state.Owner))
+        {
+            GiveDebuff(collision.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!entity.isAttached || !entity.isOwner) return;
+        BoltEntity otherEntity = other.GetComponent<BoltEntity>();
+        if (other.tag != "Room" && GetComponent<CollisionCheck>().ValidCollision(other) && otherEntity && (damageOwner || otherEntity != state.Owner))
+        {
+            GiveDebuff(other.gameObject);
+        }
+    }
+
+    private void GiveDebuff(GameObject target)
+    {
+        if (target.tag != "Player") return;
+        PlayerDebuffHandler debuffHandler = target.GetComponent<PlayerDebuffHandler>();
+        ProjectileDebuff debuff = new ProjectileDebuff();
+        if (entity.isOwner)
+        {
+            // Only include for the player that dealt this damage.
+            debuff.name = debuffName;
+            debuff.EffectLength = debuffDuration;
+            debuff.healthMod = healthModifier;
+            debuff.projectileMod = projectileSpeedModifier;
+            debuff.damageMod = damageModifier;
+            debuff.speedMod = speedModifier;
+            debuff.firerateMod = fireRateModifier;
+            debuffHandler.GrantDebuff(debuff);
+        }
+    }
+}
