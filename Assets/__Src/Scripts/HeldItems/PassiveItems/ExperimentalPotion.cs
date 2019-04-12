d4422da4d48b3e6b383c2604b060873f311590ca
grumbleBee <s.kyne96@gmail.com>
2019-04-12 09:20:08 -0400

    item implementations

--- Assets/__Src/Scripts/HeldItems/PassiveItems/ExperimentalPotion.cs
+++ Assets/__Src/Scripts/HeldItems/PassiveItems/ExperimentalPotion.cs
@@ -4,5 +4,34 @@ using UnityEngine;
 
 public class ExperimentalPotion : HeldPassive
 {
+    public AudioClip sound;
+    public AudioSource audioSource;
+    bool ready;
 
+    private void Awake(){
+        audioSource = gameObject.AddComponent<AudioSource>();
+        ready = false;
+    }
+
+    public void Update(){
+        if(ready && !audioSource.isPlaying){
+            base.OnEquip();
+        }
+    }
+
+    public override void OnEquip() {
+        if (sound != null) {
+            audioSource.Stop();
+            audioSource.clip = sound;
+            audioSource.time = 0f;
+            audioSource.Play();
+        }
+
+        Owner.state.Speed += Random.Range(-3, 3) / 10f;
+        Owner.state.FireRate += Random.Range(-3, 3) / 10f;
+        Owner.state.ProjectileSpeed += Random.Range(-3, 3) / 10f;
+        Owner.state.ProjectileDamage += Random.Range(-3, 3) / 10f;
+
+    }
 }
+
