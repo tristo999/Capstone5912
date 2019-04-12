f55d76dd0e4b2f44231995c39db76ab489fcb7d5
grumbleBee <s.kyne96@gmail.com>
2019-04-11 22:51:06 -0400

    we've all had a lot of fun here today but clipping through the map is no joke
    reigned in the potion stat changes a bit

--- Assets/__Src/Scripts/HeldItems/PassiveItems/Potion.cs
+++ Assets/__Src/Scripts/HeldItems/PassiveItems/Potion.cs
@@ -5,20 +5,23 @@ using UnityEngine;
 public class Potion : HeldPassive
 {
     public override void OnEquip() {
-        int stat = Random.Range(0, 4);
+        int stat = Random.Range(0, 5);
         
         switch(stat){
             case 0:
-                Owner.state.Speed += 5f;
+                Owner.state.Speed += 0.1f;
                 break;
             case 1:
-                Owner.state.FireRate += 5f;
+                Owner.state.FireRate += 0.25f;
                 break;
             case 2:
-                Owner.state.ProjectileSpeed += 5f;
+                Owner.state.ProjectileSpeed += 0.3f;
                 break;
             case 3:
-                Owner.state.ProjectileDamage += 5f;
+                Owner.state.ProjectileDamage += 0.15f;
+                break;
+            case 4:
+                Owner.state.Health += 5f;
                 break;
             default:
                 Owner.state.Health += 5f;
