10cffad36799a71d6a88a98a43b4f4a2e93f64f1
Brandon Pagan <pagan.48@buckeyemail.osu.edu>
2019-04-12 12:33:06 -0400

    Louder gets louder every shot

--- Assets/__Src/Scripts/HeldItems/Weapons/Louder.cs
+++ Assets/__Src/Scripts/HeldItems/Weapons/Louder.cs
@@ -2,25 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(WeaponCooldown))]
+[RequireComponent(typeof(WeaponLaunchProjectile))]
+[RequireComponent(typeof(WeaponUses))]
+
 public class Louder : Weapon
 {
-    public override void FireDown() {
-        
+    private WeaponCooldown cooldown;
+    private WeaponLaunchProjectile launchProj;
+    private WeaponUses uses;
+
+
+    private void Awake()
+    {
+        cooldown = GetComponent<WeaponCooldown>();
+        launchProj = GetComponent<WeaponLaunchProjectile>();
+    }
+    public override void FireDown()
+    {
+        if (!Owner.entity.isOwner) return;
+        if (cooldown.Ready)
+        {
+            launchProj.Launch();
+            uses.Use();
+            cooldown.ResetCooldown();
+            launchProj.LaunchForce += 15; 
+        }
     }
 
-    public override void FireHold() {
-        
-    }
-
-    public override void FireRelease() {
-        
-    }
-
-    public override void OnEquip() {
-        
-    }
-
-    public override void OnDequip() {
+    public override void FireHold()
+    {
 
     }
+
+    public override void FireRelease()
+    {
+
+    }
+
+    public override void OnEquip()
+    {
+
+    }
+
+    public override void OnDequip() { }
 }
