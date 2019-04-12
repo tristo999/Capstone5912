960f8ddbc64aa397b5091ff5bca5d2c328b36520
Brandon Pagan <pagan.48@buckeyemail.osu.edu>
2019-04-12 10:46:08 -0400

    Louder weapon

---
+++ Assets/__Src/Scripts/HeldItems/Weapons/Louder.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Louder : Weapon
+{
+    public override void FireDown() {
+        
+    }
+
+    public override void FireHold() {
+        
+    }
+
+    public override void FireRelease() {
+        
+    }
+
+    public override void OnEquip() {
+        
+    }
+
+    public override void OnDequip() {
+
+    }
+}
