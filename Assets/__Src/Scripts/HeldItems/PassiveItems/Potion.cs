38b8f8dc56c977791b0f77a9bbdffddd472ae0a5
grumbleBee <s.kyne96@gmail.com>
2019-04-11 22:32:49 -0400

    pull -> compile -> push -> ??? -> profit!
    can't fix my life but i CAN fix the bolt assets!

--- Assets/__Src/Scripts/HeldItems/PassiveItems/Potion.cs
+++ Assets/__Src/Scripts/HeldItems/PassiveItems/Potion.cs
@@ -4,5 +4,27 @@ using UnityEngine;
 
 public class Potion : HeldPassive
 {
-
+    public override void OnEquip() {
+        int stat = Random.Range(0, 4);
+        
+        switch(stat){
+            case 0:
+                Owner.state.Speed += 5f;
+                break;
+            case 1:
+                Owner.state.FireRate += 5f;
+                break;
+            case 2:
+                Owner.state.ProjectileSpeed += 5f;
+                break;
+            case 3:
+                Owner.state.ProjectileDamage += 5f;
+                break;
+            default:
+                Owner.state.Health += 5f;
+                break;
+        }
+        
+        base.OnEquip();
+    }
 }
