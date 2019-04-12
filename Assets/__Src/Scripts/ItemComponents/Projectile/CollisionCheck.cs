d8617fd048a34604c2cc5da1cd52aa45c430bb70
rcologgi <cologgi.2@buckeyemail.osu.edu>
2019-04-11 23:05:21 -0400

    Fixed self collision issues
    Throwing knives work again (unless you clip the ground from how they're thrown)

--- Assets/__Src/Scripts/ItemComponents/Projectile/CollisionCheck.cs
+++ Assets/__Src/Scripts/ItemComponents/Projectile/CollisionCheck.cs
@@ -9,26 +9,25 @@ public class CollisionCheck : Bolt.EntityBehaviour<IProjectileState>
     public bool CollideWithEnvironment;
 
     public bool ValidCollision(Collision collision) {
-
         BoltEntity collidedEntity = collision.gameObject.GetComponent<BoltEntity>();
-        bool playerCheck = collision.gameObject.tag == "Player" && CollideWithPlayers;
+        bool playerCheck = collision.gameObject.tag == "Player" && state.Owner != collidedEntity && CollideWithPlayers;
         bool selfCheck = state.Owner == collidedEntity && CollideWithOwner;
         bool envCheck = collision.gameObject.tag != "Player" && CollideWithEnvironment;
-        bool rangeColliderCheck = collision.gameObject.tag != "RangeDetector";
-        bool projectileCheck = collision.gameObject.tag != "Projectile";
+        bool rangeColliderCheck = collision.gameObject.tag == "RangeDetector";
+        bool projectileCheck = collision.gameObject.tag == "Projectile";
+        // if (playerCheck || selfCheck || envCheck || rangeColliderCheck || projectileCheck) Debug.Log($"{collision.gameObject.name} collider| playerCheck: {playerCheck} selfCheck: {selfCheck} envCheck: {envCheck} rangeCol: {rangeColliderCheck} projectileCheck: {projectileCheck}");
         return playerCheck || selfCheck || envCheck || rangeColliderCheck || projectileCheck;
     }
 
     public bool ValidCollision(Collider collider) {
-        
         BoltEntity collidedEntity = collider.gameObject.GetComponent<BoltEntity>();
-        bool playerCheck = collider.gameObject.tag != "Player" || CollideWithPlayers;
+        bool playerCheck = collider.gameObject.tag != "Player" || state.Owner == collidedEntity || CollideWithPlayers;
         bool selfCheck = state.Owner != collidedEntity || CollideWithOwner;
-        bool envCheck = collider.gameObject.tag != "Player" ||  CollideWithEnvironment;
+        bool envCheck = collider.gameObject.tag != "Player" || CollideWithEnvironment;
         bool rangeColliderCheck = collider.gameObject.tag != "RangeDetector";
         bool projectileCheck = collider.gameObject.tag != "Projectile";
         bool roomTriggerCheck = collider.gameObject.tag != "Room";
+        // if (playerCheck && selfCheck && envCheck && rangeColliderCheck && projectileCheck && roomTriggerCheck) Debug.Log($"{collider.gameObject.name} collision| playerCheck: {!playerCheck} selfCheck: {!selfCheck} envCheck: {!envCheck} rangeCol: {!rangeColliderCheck} projectileCheck: {!projectileCheck}");
         return playerCheck && selfCheck && envCheck && rangeColliderCheck && projectileCheck && roomTriggerCheck;
-
     }
 }
