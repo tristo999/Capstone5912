c17728b51ea7066fc23cc8930829c22b3857e9ee
rcologgi <cologgi.2@buckeyemail.osu.edu>
2019-04-17 03:52:57 -0400

    Added message feedback for garbage.

--- Assets/__Src/Scripts/HeldItems/ActiveItems/Garbage.cs
+++ Assets/__Src/Scripts/HeldItems/ActiveItems/Garbage.cs
@@ -13,12 +13,27 @@ public class Garbage : ActiveItem
     }
 
     public override void ActiveDown() {
+        int randomMessageId = Random.Range(0, 4);
+        string randomMessage = "";
 
+        if (randomMessageId == 0) {
+            randomMessage = "Grossss!";
+        } else if (randomMessageId == 1) {
+            randomMessage = "Ewww! Don't play with it!";
+        } else if (randomMessageId == 2) {
+            randomMessage = "You're disgusting";
+        } else if (randomMessageId == 3) {
+            randomMessage = "Why did you try that";
+        }
+
+        Owner.GetComponent<PlayerStatsController>().ui.AddFloatingMessageText(randomMessage, Owner.transform.position);
     }
 
     public override void OnEquip() {
-
+        Owner.GetComponent<PlayerStatsController>().ui.AddFloatingMessageText("Nooo don't pick that up!", Owner.transform.position);
     }
 
-    public override void OnDequip() { }
+    public override void OnDequip() {
+        Owner.GetComponent<PlayerStatsController>().ui.AddFloatingMessageText("I need a shower...", Owner.transform.position);
+    }
 }
