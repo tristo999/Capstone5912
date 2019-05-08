efd4bee8138953fb309cf27d66fd09a2ab257dd3
grumbleBee <s.kyne96@gmail.com>
2019-05-07 20:39:51 -0400

    Revert "reasonably sure this didn't break anything"
    This reverts commit 752720b47b88db65b3557f95e8b923528d4cb65d.

---
+++ Assets/bpTest/BrandonEnemyHealth.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BrandonEnemyHealth : MonoBehaviour { 
+
+    public Transform healthBar;
+    public Slider healthFill;
+
+    public float currentHealth;
+    public float maxHealth;
+    public float healthBarYOffset = 2;
+
+
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth += amount;
+        //make health value stay between 0 and max
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        //percentage for slider fill
+        healthFill.value = currentHealth / maxHealth;
+    }
+
+    //set pos of healthbar above parent object
+    private void HealthBarPos()
+    {
+        Vector3 currentPos = transform.position;
+        healthBar.position = new Vector3(currentPos.x, currentPos.y + healthBarYOffset, currentPos.z);
+
+        //show health bar to camera
+        healthBar.LookAt(Camera.main.transform);
+    }
+
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        HealthBarPos();   
+    }
+}
---
+++ Assets/bpTest/EnemyMove.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    Animator anim;
+    int speedHash, turnHash, comboHash, numClicks;
+    Vector3 position, forward = Vector3.forward;
+    public float speed;
+    bool canAttack = true;
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        speedHash = Animator.StringToHash("Speed");
+        turnHash = Animator.StringToHash("Turn");
+        comboHash = Animator.StringToHash("ComboNum");
+
+    }
+
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Attack();
+        }
+
+
+        float rotateY = Input.GetAxis("Horizontal") * speed;
+
+        float moveVertical = Input.GetAxis("Vertical");
+
+        if (moveVertical < 0)
+            moveVertical *= -1;
+
+
+        anim.SetFloat(speedHash, moveVertical);
+
+        transform.eulerAngles += new Vector3(0.0f, rotateY, 0.0f);
+        
+        gameObject.transform.Translate(forward *Time.deltaTime * moveVertical * speed);
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+       
+    }
+
+    public void Attack()
+    {
+       
+
+        if (canAttack)
+        {
+            numClicks++;
+            anim.SetInteger(comboHash, numClicks);
+
+        }
+
+        if (numClicks >= 3)
+            numClicks = 0;
+    }
+
+   
+
+  
+
+    private IEnumerator playAnimation(int seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+    }
+
+}
