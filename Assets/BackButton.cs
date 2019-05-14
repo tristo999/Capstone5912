351d7fa6bee1b93f2f7d996fdb937a53d549215f
grumbleBee <s.kyne96@gmail.com>
2019-05-14 19:43:02 -0400

    added "return to menu" button to lobby

---
+++ Assets/BackButton.cs
@@ -0,0 +1,31 @@
+﻿using Rewired;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class BackButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ReturnToMenu() {
+        if (BoltNetwork.IsServer) {
+            foreach (BoltConnection client in BoltNetwork.Clients) {
+                client.Disconnect();   
+            }
+            BoltLauncher.Shutdown();
+        } else {
+            DisconnectPlayer.Create().Send();
+        }
+        SceneManager.LoadScene(1);
+    }
+}
