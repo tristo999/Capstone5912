4985f2878f73f899ce9441d8b8de2b94bbd8bc79
grumbleBee <s.kyne96@gmail.com>
2019-10-11 22:55:13 -0400

    rewired support for main menu

--- Assets/__Src/Scripts/MainMenu/MainMenuController.cs
+++ Assets/__Src/Scripts/MainMenu/MainMenuController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using DG.Tweening;
 using TMPro;
 using UnityEngine.SceneManagement;
+using Rewired;
+using UnityEngine.EventSystems;
 
 public class MainMenuController : MonoBehaviour
 {
@@ -20,77 +22,112 @@ public class MainMenuController : MonoBehaviour
 
     private bool onMenu;
     private AudioSource source;
+    private IList<Player> players;
+    public EventSystem eventSystem;
 
 
     private void Start()
     {
         source = GetComponent<AudioSource>();
+        players = ReInput.players.GetPlayers();
        
+    }
+
+    bool getAnyButtonDown(IList<Player> players){
+        foreach(Player player in players){
+            if(player.GetAnyButtonDown()) return true;
+        }
+        return false;
     }
 
     // Did you know: Update is called once per frame
     void Update()
     {
-        if (Input.anyKeyDown && !onMenu)
+        if (getAnyButtonDown(players) && !onMenu)
         {
             pressAnyFade.DOKill();
             pressAnyFade.GetComponent<TextMeshProUGUI>().DOFade(0, .2f);
             TopMenu.DOAnchorPosX(0, 1.0f);
             BottomMenu.DOAnchorPosX(0, 1.0f);
             Title.DOFade(1f, 0.5f);
+            eventSystem.SetSelectedGameObject(GameObject.Find("Top Menus/Play"));
             onMenu = true;
         }
     }
 
     public void OptionsTransition()
     {
+        OptionsMenu.gameObject.SetActive(true);
+        eventSystem.SetSelectedGameObject(GameObject.Find("Options Menu/Sliders/Master"));
         source.PlayOneShot(click);
         TopMenu.DOAnchorPosX(-1100, 1.0f);
         BottomMenu.DOAnchorPosX(1100, 1.0f);
         Title.DOFade(0f, 0.5f);
         OptionsMenu.DOAnchorPosX(0f, 1.0f);
+        TopMenu.gameObject.SetActive(false);
+        BottomMenu.gameObject.SetActive(false);
     }
 
     public void OptionsExitTransition(){
+        TopMenu.gameObject.SetActive(true);
+        BottomMenu.gameObject.SetActive(true);
+        eventSystem.SetSelectedGameObject(GameObject.Find("Top Menus/Play"));
         source.PlayOneShot(click);
         TopMenu.DOAnchorPosX(0, 1.0f);
         BottomMenu.DOAnchorPosX(0, 1.0f);
         Title.DOFade(1f, 0.5f);
         OptionsMenu.DOAnchorPosX(2000f, 1.0f);
+        OptionsMenu.gameObject.SetActive(false);
     }
 
     public void CreditsFlyIn()
     {
+        Credits.gameObject.SetActive(true);
+        eventSystem.SetSelectedGameObject(GameObject.Find("Credits Menu/Back Button"));
         source.PlayOneShot(click);
         TopMenu.DOAnchorPosX(-1100, 1.0f);
         BottomMenu.DOAnchorPosX(1100, 1.0f);
         Title.DOFade(0f, 0.5f);
         Credits.DOAnchorPosY(0f, 2f);
+        TopMenu.gameObject.SetActive(false);
+        BottomMenu.gameObject.SetActive(false);
     }
 
     public void CreditsFlyOut()
     {
+        TopMenu.gameObject.SetActive(true);
+        BottomMenu.gameObject.SetActive(true);
+        eventSystem.SetSelectedGameObject(GameObject.Find("Top Menus/Play"));
         source.PlayOneShot(click);
         TopMenu.DOAnchorPosX(0, 1.0f);
         BottomMenu.DOAnchorPosX(0, 1.0f);
         Title.DOFade(1f, 0.5f);
         Credits.DOAnchorPosY(1100f, 1.0f);
+        Credits.gameObject.SetActive(false);
     }
 
     public void PlayOptionsFlyIn(){
+        PlayOptions.gameObject.SetActive(true);
+        eventSystem.SetSelectedGameObject(GameObject.Find("Play Buttons/PlayLocal"));
         source.PlayOneShot(click);
         TopMenu.DOAnchorPosX(-1100, 1.0f);
         BottomMenu.DOAnchorPosX(1100, 1.0f);
         PlayOptions.DOAnchorPosY(-75f, 1.0f);
+        TopMenu.gameObject.SetActive(false);
+        BottomMenu.gameObject.SetActive(false);
     }
 
     
     public void PlayOptionsFlyOut()
     {
+        TopMenu.gameObject.SetActive(true);
+        BottomMenu.gameObject.SetActive(true);
+        eventSystem.SetSelectedGameObject(GameObject.Find("Top Menus/Play"));
         source.PlayOneShot(click);
         TopMenu.DOAnchorPosX(0, 1.0f);
         BottomMenu.DOAnchorPosX(0, 1.0f);
         PlayOptions.DOAnchorPosY(-1100f, 1.0f);
+        PlayOptions.gameObject.SetActive(false);
     }
 
     public void QuitGame()
